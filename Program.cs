03a567fdf0adb5faeee67fe91b73a19eaacd3eb2
Ilya Maksimov <iluxamax@yandex.ru>
2023-12-27 15:42:23 +0300

    Finish

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Пример произвольного массива
+        int[] myArray = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Элементы массива в обратном порядке:");
+        PrintArrayReversed(myArray, myArray.Length - 1);
+
+        // Задержка экрана
+        Console.ReadLine();
+    }
+
+    static void PrintArrayReversed(int[] arr, int index)
+    {
+        // Базовый случай: если индекс выходит за границы массива, выходим из рекурсии
+        if (index < 0)
+            return;
+
+        // Выводим элемент массива
+        Console.WriteLine(arr[index]);
+
+        // Рекурсивный вызов для следующего элемента в обратном порядке
+        PrintArrayReversed(arr, index - 1);
+    }
+}
